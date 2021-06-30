db6b4c57e2ae233203d74f7f1fa359475642bddd
orozcohsu <orozcohsu@hotmail.com>
2021-06-30 12:32:00 -0700

    update

--- 20210730/hmaster/run.sh
+++ 20210730/hmaster/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 nohup /opt/hbase-$HBASE_VERSION/bin/hbase rest start -p 4444 &
+nohup /opt/hbase-$HBASE_VERSION/bin/hbase thrift start &
 /opt/hbase-$HBASE_VERSION/bin/hbase master start
 
